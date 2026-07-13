843cc93724bac6dfab1e5307d2b9d1d129f2bd1a
Boxxedboi <minefooox@gmail.com>
2026-07-12 19:34:58 -0600

    Update FTS to 4.0

--- src/main/java/moth/boxxed/panels/index/PanelCreativeTabs.java
+++ src/main/java/moth/boxxed/panels/index/PanelCreativeTabs.java
@@ -33,54 +33,24 @@ public class PanelCreativeTabs {
                     .build()
     );
 
-    public static final Map<Supplier<CreativeModeTab>, Set<ItemLike>> creativeItemMap = new HashMap<>();
-    public static void addContentTo(Supplier<CreativeModeTab> tab, ItemLike item) {
-        Set<ItemLike> set = creativeItemMap.computeIfAbsent(tab, $ -> new LinkedHashSet<>());
-        set.add(item);
-    }
-
     public static void addItems() {
-        ConglomerateOfItems base = new ConglomerateOfItems()
-                .add(PanelBlocks.CONTROL_PANEL)
-                .add(PanelBlocks.CABLE)
-                .add(PanelItems.CABLE_STRIPPER);
-        if (ModList.get().isLoaded("create"))
-            base.add(PanelCreateRegistries.PANEL_LINK);
-//        FancyTabSections.addSection(Dashpanels.path("dashpanels"), SectionTextured.of(
-//                Dashpanels.path("dashpanels"),
-//                Component.translatable("creativetab.dashpanels.dashpanels"),
-//                0xFFFFFF,
-//                base
-//        ));
-        FancyTabSections.addSection(Dashpanels.path("dashpanels"), CustomSectionTextured.of(
-                Dashpanels.path("dashpanels"),
-                Component.translatable("creativetab.dashpanels.dashpanels"),
-                0xFFFFFF,
-                true,
-                true,
-                base
-        ));
+        SectionTextured defaultSection = new SectionTextured(Dashpanels.path("dashpanels"));
+        defaultSection.setTitle(Component.translatable("creativetab.dashpanels.dashpanels"));
 
-        ConglomerateOfItems modules = new ConglomerateOfItems()
-                .add(PanelItems.SWITCH_MODULE)
-                .add(PanelItems.KNOB_MODULE)
-                .add(PanelItems.CONTROL_LEVER_MODULE)
-                .add(PanelItems.INDICATOR_BULB_MODULE)
-                .add(PanelItems.MOMENTARY_SWITCH_MODULE)
-                .add(PanelItems.JOYSTICK_MODULE)
-                .add(PanelItems.LABEL_MODULE)
-                .add(PanelItems.SEVEN_SEGMENT_MODULE);
-        if (ModList.get().isLoaded("sable"))
-            modules.add(PanelSableRegistries.NAVBALL_MODULE);
-        FancyTabSections.addSection(Dashpanels.path("dashpanels"), new CustomSectionTextured(
-                Dashpanels.path("modules"),
-                Component.translatable("creativetab.dashpanels.modules"),
-                Dashpanels.path("textures/gui/fancy_tab_section/dashpanels.png"),
-                0xFFFFFF,
-                true,
-                true,
-                modules
-        ));
+        defaultSection.add(PanelBlocks.CONTROL_PANEL);
+        defaultSection.add(PanelBlocks.CABLE);
+        defaultSection.add(PanelItems.CABLE_STRIPPER);
+        if (ModList.get().isLoaded("create"))
+            defaultSection.add(PanelCreateRegistries.PANEL_LINK);
+        FancyTabSections.addSection(Dashpanels.path("dashpanels"), defaultSection
+        );
+
+        SectionTextured modulesSection = new SectionTextured(Dashpanels.path("dashpanels"));
+        modulesSection.setTitle(Component.translatable("creativetab.dashpanels.modules"));
+        modulesSection.setTexture(Dashpanels.path("dashpanels"));
+
+        modulesSection.addItemTag(PanelTags.Items.MODULE);
+        FancyTabSections.addSection(Dashpanels.path("dashpanels"), modulesSection);
     }
 
     public static void register(IEventBus eventBus) {
